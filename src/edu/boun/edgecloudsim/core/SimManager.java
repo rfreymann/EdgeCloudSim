4a0329a7159025ed50dfbf0a8a5910d6ec92d325
Trenomor <raphael.freymann@outlook.com>
2021-01-05 13:46:06 +0100

    more tsting

--- src/edu/boun/edgecloudsim/core/SimManager.java
+++ src/edu/boun/edgecloudsim/core/SimManager.java
@@ -246,8 +246,10 @@ public class SimManager extends SimEntity {
 				break;
 			case PRINT_PROGRESS:
 				int progress = (int)((CloudSim.clock()*100)/SimSettings.getInstance().getSimulationTime());
-				if(progress % 10 == 0)
+				if(progress % 10 == 0) {
+					SimLogger.printLine(CloudSim.getFuture().size() + ":" + CloudSim.getDeferred().size());
 					SimLogger.print(Integer.toString(progress));
+				}
 				else
 					SimLogger.print(".");
 				if(CloudSim.clock() < SimSettings.getInstance().getSimulationTime())
