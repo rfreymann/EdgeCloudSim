fd6de00b43fe8078ccca47c4ff197888d176a96e
Trenomor <raphael.freymann@outlook.com>
2021-01-16 12:02:25 +0100

    mobility model fixes

--- src/edu/boun/edgecloudsim/utils/Location.java
+++ src/edu/boun/edgecloudsim/utils/Location.java
@@ -1,8 +1,8 @@
 /*
  * Title:        EdgeCloudSim - Location
- * 
+ *
  * Description:  Location class used in EdgeCloudSim
- * 
+ *
  * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
  * Copyright (c) 2017, Bogazici University, Istanbul, Turkey
  */
@@ -14,37 +14,39 @@ public class Location {
 	private int yPos;
 	private int servingWlanId;
 	private int placeTypeIndex;
-	public Location(int _placeTypeIndex, int _servingWlanId){
+	public Location(int _placeTypeIndex, int _servingWlanId, int _xPos, int _yPos){
 		servingWlanId = _servingWlanId;
 		placeTypeIndex=_placeTypeIndex;
+		xPos = _xPos;
+		yPos = _yPos;
 	}
-	
+
 	@Override
 	public boolean equals(Object other){
 		boolean result = false;
-	    if (other == null) return false;
-	    if (!(other instanceof Location))return false;
-	    if (other == this) return true;
-	    
-	    Location otherLocation = (Location)other;
-	    if(servingWlanId == otherLocation.getServingWlanId() && placeTypeIndex == otherLocation.getPlaceTypeIndex())
-	    	result = true;
+		if (other == null) return false;
+		if (!(other instanceof Location))return false;
+		if (other == this) return true;
 
-	    return result;
+		Location otherLocation = (Location)other;
+		if(this.xPos == otherLocation.xPos && this.yPos == otherLocation.yPos)
+			result = true;
+
+		return result;
 	}
 
 	public int getServingWlanId(){
 		return servingWlanId;
 	}
-	
+
 	public int getPlaceTypeIndex(){
 		return placeTypeIndex;
 	}
-	
+
 	public int getXPos(){
 		return xPos;
 	}
-	
+
 	public int getYPos(){
 		return yPos;
 	}
